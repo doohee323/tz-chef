58aee5a07f3e45864c161f6dc7d6632f040c3e98
Dewey Hong <dhong@fortinet.com>
2017-04-01 23:40:48 -0700

    .

--- resources/chef-client/chef-repo/cookbooks_test/sample/recipes/default.rb
+++ resources/chef-client/chef-repo/cookbooks_test/sample/recipes/default.rb
@@ -1,10 +1,21 @@
-vip_ip_addresses = []
 dev_db_hash = data_bag_item("test", "dev").to_hash
-vip_data = dev_db_hash["test"]["dev"]
+
+Chef::Log.warn "=====================test: #{dev_db_hash["test"]} ================="
+Chef::Log.warn "=====================test/dev: #{dev_db_hash["test"]["dev"]} ================="
+dev1 = dev_db_hash["test"]["dev"]
+Chef::Log.warn "=====================dev1: #{dev1} ================="
+Chef::Log.warn "=====================dev1: #{dev1["bind_ip"]} ================="
+
+dev = data_bag('test')
+Chef::Log.warn "=====================dev: #{dev} ================="
+
+dev2 = data_bag_item('test', 'dev')
+Chef::Log.warn "=====================dev2: #{dev2} ================="
+Chef::Log.warn "=====================ids: #{dev2["id"]} ================="
 
 template "/tmp/herp.conf" do
   source "herp.conf.erb"
-  variables :username => "myapp", :password => vip_data["bind_ip"]
+  variables :username => "myapp", :password => dev1["bind_ip"]
   mode "0644"
 end
 
